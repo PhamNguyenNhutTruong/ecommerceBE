42562bf789cd98c32835de8134be42bea13e2202
DODUONGTHAITUAN <doduongthaituan201102@gmail.com>
2023-01-28 08:37:48 +0700

    refactor src code

--- src/main/java/project/ute/controller/SizeController.java
+++ src/main/java/project/ute/controller/SizeController.java
@@ -1,5 +1,7 @@
 package project.ute.controller;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -7,6 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import project.ute.service.SizeService;
@@ -18,8 +21,8 @@ public class SizeController {
 	SizeService sizeService;
 
 	@RequestMapping(value = "/size/show-all", method = RequestMethod.GET, produces = { MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE })
-	public ResponseEntity<?> showAllSize() {
-		return ResponseEntity.status(HttpStatus.OK).body(sizeService.getAllSize());
+	public ResponseEntity<?> showAllSize(@RequestParam("pageNo")Optional<Integer> pageNo) {
+		return ResponseEntity.status(HttpStatus.OK).body(sizeService.paging(pageNo));
 	}
 
 	@RequestMapping(value = "/size/show-with-size", method = RequestMethod.GET, produces = { MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE })
@@ -27,3 +30,4 @@ public class SizeController {
 		return ResponseEntity.status(HttpStatus.OK).body(sizeService.getSizeByName("M", 20000));
 	}
 }
+	
--- src/main/java/project/ute/service/SizeService.java
+++ src/main/java/project/ute/service/SizeService.java
@@ -1,23 +1,12 @@
 package project.ute.service;
 
 import java.util.List;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
+import java.util.Optional;
 
 import project.ute.model.Size;
-import project.ute.respository.SizeRepository;
 
-@Service
-public class SizeService {
-	@Autowired
-	private SizeRepository sizeRepository;
-	
-	public List<Size> getAllSize() {
-		return sizeRepository.findAll();
-	}
-	
-	public List<Size> getSizeByName(String name, long price) {
-		return sizeRepository.getSizeByName(name, price);
-	}
+public interface SizeService {
+	List<Size> getAllSize();
+	List<Size> paging(Optional<Integer> pageNo);
+	List<Size> getSizeByName(String name, long price);
 }
